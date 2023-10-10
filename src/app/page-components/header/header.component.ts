875792cccf888ad89b048d3617ab878a5bc009ec
aleks-thunder <aleks.thunder.m@gmail.com>
2023-10-10 17:38:44 +0300

    header feature

--- src/app/page-components/header/header.component.ts
+++ src/app/page-components/header/header.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  scrollToElement(elementId: string) {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+}
